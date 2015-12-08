1c04c597175c186f7e337573b58a1bc4ee7b870f
nrusitska <n.rusitska@googlemail.com>
2015-12-08 21:30:13 +0100

    SQLite verweise korrekt. Aufruf-Wrapping für MAME

--- RadXAutomat/RadXAutomat.Main/Form1.cs
+++ RadXAutomat/RadXAutomat.Main/Form1.cs
@@ -1,19 +1,40 @@
-﻿using System;
+﻿using RadXAutomat.Data;
+using RadXAutomat.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace RadXAutomat.Main
 {
     public partial class Form1 : Form
     {
+        InteractionManager _manager = new InteractionManager();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _manager.StartGame1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _manager.StartGame2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _manager.StartGame3();
         }
     }
 }
